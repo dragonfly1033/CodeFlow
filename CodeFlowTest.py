0edc3d7322ccc11476a804364b0f006a219a49e8
shrey patel <shreyrg14@gmail.com>
2020-11-17 12:46:24 +0000

    update

--- CodeFlowTest.py
+++ CodeFlowTest.py
@@ -1,10 +1,15 @@
+from re import findall as find
 
-dict = {'imports0': 'CodeFlowInterpreter', 'imports1': 'CodeFlowTest', 'defvars4': ['a', '9'], 'defvars6': ['b', '8'], 'prints8': 'a+b', 'defvars10': ['c', "'hey'"], 'defvars12': [
-    'd', 'a+b'], 'prints14': 'c, d', 'defvars16': ['g', "input('whats your name: ')"], 'inputs16': "'whats your name: '", 'other19': 'CodeFlowInterpreter.Flow(__file__)\n'}
+def parse(lines):
+  unclosed = {}
 
-
-keys = list(dict.keys())
-
-for i in dict:
-    if('input' in dict[i][1]):
-        print(keys[keys.index(i)+1])
+  for no, rawLine in enumerate(lines):
+    line = rawLine.strip()
+    fors = find(r'for (\w+\s*,?\s*\w*) in (enumerate\((.+)\))?(?:range\((.+)\))?(.+)?:', line)
+    whiles = find(r'while\s+(.+):', line)
+    ifs = find(r'(if|elif|else)(?:\s*\(?(.+)\)?\s*:)', line)
+    prints = find(r'print\((.*)\)', line)
+    functions = find(r'def\s+(\w+)\((.*)\):', line)
+    imports = find(r'import (.+)', line)
+    inputs = find(r'input\((.*)\)', line)
+    defVars = find(r'(\w+)\s*=\s*(.+)', line)
