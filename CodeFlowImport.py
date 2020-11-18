0d8c4d9e32af2773225fb6f5dbe16cf3cbb3fbc1
shrey patel <shreyrg14@gmail.com>
2020-11-18 14:56:16 +0000

    everything

--- CodeFlowImport.py
+++ CodeFlowImport.py
@@ -4,14 +4,15 @@ a = 5
 b = '4'
 c=a+int(b)
 print(c)
+list=[1,2,3,4,5]
 if c == 9:
   print('square!')
 
 while 1 == 9:
   pass
 
-for i, v in enumerate([1,2,3,4,5]):
-  print(i, v)
+for i in range(b, c):
+  print(i)
 
 
 CodeFlowInterpreter.Flow(__file__)
--- CodeFlowTest.py
+++ CodeFlowTest.py
@@ -1,6 +1,7 @@
 from re import findall, sub
 
 def preParse(lines):
+  global indents
   clean = []
   indents = []
 
@@ -22,41 +23,33 @@ def parse(lines):
 
   for no, rawLine in enumerate(lines):
     line = rawLine.strip()
-    fors = findall(r'for (\w+\s*,?\s*\w*) in (enumerate\((.+)\))?(?:range\((.+)\))?(.+)?:', line)
-    whiles = findall(r'while\s+(.+):', line)
-    ifs = findall(r'(if|elif|else)(?:\s*\(?(.+)\)?\s*:)', line)
-    prints = findall(r'print\((.*)\)', line)
-    functions = findall(r'def\s+(\w+)\((.*)\):', line)
-    imports = findall(r'import (.+)', line)
-    inputs = findall(r'input\((.*)\)', line)
-    defVars = findall(r'(\w+)\s*=\s*([^=]+)', line)
-    #print(f'fors: {fors}')
-    #print(f'whiles: {whiles}')
-    #print(f'ifs: {ifs}')
-    #print(f'prints: {prints}')
-    #print(f'functions: {functions}')
-    #print(f'imports: {imports}')
-    #print(f'inputs: {inputs}')
-    #print(f'defVars: {defVars}')
-    #print()
-    if(fors):
-      pass
-    elif(whiles):
-      pass
-    elif(ifs):
-      pass
-    elif(prints):
-      pass
-    elif(functions):
-      pass
-    elif(imports):
-      pass
-    elif(inputs):
-      pass
-    elif(defVars):
-      pass
-    else:
-      other = [line]
+    found = {}
+    found['fors'] = findall(r'for (\w+\s*,?\s*\w*) in (enumerate\((.+)\))?(?:range\((.+)\))?(.+)?:', line)
+    found['whiles'] = findall(r'while\s+(.+):', line)
+    found['ifs'] = findall(r'(if|elif|else)(?:\s*\(?(.+)\)?\s*:)', line)
+    found['prints'] = findall(r'print\((.*)\)', line)
+    found['functions'] = findall(r'def\s+(\w+)\((.*)\):', line)
+    found['imports'] = findall(r'import (.+)', line)
+    found['inputs'] = findall(r'input\((.*)\)', line)
+    found['defVars'] = findall(r'(\w+)\s*=\s*([^=]+)', line)
+    found['others'] = []
+    if(sum([len(i) for i in found.values()]) == 0):
+      found['others'] = [line]
+    construct = [i for i in found.values() if i][0][0]
+    if isinstance(construct, tuple): construct = {'info': list(construct)}
+    else: construct = {'info': [construct]}
+    if found['fors']: construct['info'] = [i for i in construct['info'] if i]
+    for i in found:
+      if found[i]:
+        construct['type'] = i[:-1]
+
+    for i in indents:
+      if construct['type'] in ['for', 'while', 'if', 'function']:
+        pass
+      construct['open'] = no
+      construct['indent'] = indents[no]
+
+    print(construct)
 
 def Flow():
   with open('CodeFlowImport.py', 'r') as f:
@@ -67,6 +60,8 @@ def Flow():
 nodeVals = {'fors': 3, 'whiles': 1, 'ifs': 1, 'prints': 1, 'functions': 1, 'imports': 1, 'inputs': 2, 'defVars': 1, 'other': 1}
 
 lineInfo = []
+
+curBlock = 0
 
 Flow()
     
